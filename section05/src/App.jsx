dbda08e45552ea07cf07804583a180d6e03b49c6
psh94 <suran5281@gmail.com>
2025-08-17 21:44:17 +0900

    section05: state로 상태관리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css';
-import Header from "./components/Header.jsx";
-import Main from './components/Main.jsx';
-import Footer from './components/Footer.jsx';
-import Button from './components/Button.jsx';
+import {useState} from "react";
 
 function App() {
-  const buttonProps = {
-    text:"메일",
-    color:"red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+
   return (
     <>
-      <Button {...buttonProps}/>
-      <Button text={"카페"}/>
-      <Button text={"블로그"}>
-          <div>자식 요소</div>
-          <Header/>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON");
+        }}>
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1);
+        }}>
+          +
+        </button>
+      </div>
     </>
   )
 }
