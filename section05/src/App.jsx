65c5e8af915a47c11d8c8f1650ea0e11d380e00b
psh94 <suran5281@gmail.com>
2025-08-17 10:35:08 +0900

    section05: 컴포넌트 실습

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.css';
+import Header from "./components/Header.jsx";
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
+
 
 function App() {
-
   return (
     <>
-      <h1> 안녕 리액트</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
