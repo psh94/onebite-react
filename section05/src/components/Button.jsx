3aaa10ab26159968e5b2ad07ac5727449c2d7c9f
psh94 <suran5281@gmail.com>
2025-08-17 14:55:56 +0900

    section05: Props

---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({children, text,color = "black"}) => {
+    return (
+        <button style={{color:color}}>
+            {text} - {color.toUpperCase()}
+            {children}
+        </button>
+    );
+};
+export default Button;
