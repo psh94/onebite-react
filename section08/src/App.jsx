5a1ecf41ace4fc9fdda7e9365581af29340111a1
psh94 <suran5281@gmail.com>
2025-08-23 20:59:51 +0900

    section08: 투두리스트 ui 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header.jsx'
+import Editor from './components/Editor.jsx'
+import List from './components/List.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>To Do List</div>
-    </>
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
   )
 }
 
