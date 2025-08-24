30cf3fb820ae6a53ab75a7b01818c62c3436cbd4
psh94 <suran5281@gmail.com>
2025-08-24 15:36:08 +0900

    section08: 투두리스트 기능구현, 완료

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,43 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import {useState} from "react";
 
-const List = () => {
+const List = ({ todos, onUpdate, onDelete }) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const getFilteredDate = () => {
+        if(search === ""){
+            return todos;
+        }
+        return todos.filter((todo) =>
+            todo.content
+                .toLowerCase()
+                .includes(search.toLowerCase()));
+    }
+    const filteredTodos = getFilteredDate();
+
     return (
         <div className="List">
             <h4>Todo List 📜</h4>
-            <input placeholder="검색어를 입력하세요" />
+            <input 
+                value={search}
+                onChange={onChangeSearch}
+                placeholder="검색어를 입력하세요" 
+            />
             <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {filteredTodos.map((todo) => {
+                    return (
+                        <TodoItem
+                            key={todo.id}
+                            {...todo}
+                            onUpdate={onUpdate}
+                            onDelete={onDelete} />
+                        );
+                })}
             </div>
         </div>
     )
